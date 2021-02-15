146f689c1a3a762792175d13771125572a9fcc6d
Temmy Alex <temmy.alex@gmail.com>
2021-02-15 20:06:35 +0700

    Intro to js

---
+++ index.js
@@ -0,0 +1,46 @@
+var name = 'Test';
+var age = 20;
+// Penulisan camel case
+var firstName = 'Temmy Alex';
+// Penulisan snake_case
+var last_name = 'Temmy';
+var HomeAddress = 'Street';
+
+var email;
+email = 'test@gmail.com';
+var email = 'testng@gmail.com';
+var $test = 'test';
+
+// Variable in variable
+var old_email = email;
+
+var status_success = 'SUCCESS';
+var status_success = 'success';
+// let fullname = 'casey';
+
+let fullname = 'fateh ali';
+let ageperson = 23;
+let address = "Testing";
+// Hoisting
+x = 10;
+console.log(x);
+var x;
+
+// y = 10;
+// let y;
+
+// Let
+// tidak bisa di reassign
+// tidak bisa di panggil sebelum di inisialisasi
+// masalah scope
+
+var b = 2
+if(true){
+    var b = 3
+}
+
+console.log(b);
+console.log(name);
+console.log(email);
+console.log(old_email);
+// console.log(fullname);
